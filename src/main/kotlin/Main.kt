edf17fd0f1628c8e47290cf01d6635dea3a6bbb1
bmaum321 <103905368+bmaum321@users.noreply.github.com>
2022-05-23 12:04:07 -0400

    moved to local word database
    Moved to the TWL (torunament word list used in scrabble) This should make the game much eaiser and it also verifies input is a real word

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,15 @@ import com.brian.wordle.api.ApiClient
 import com.brian.wordle.usecase.GetRandomWordUseCase
 import com.brian.wordle.usecase.RandomWordResults
 import com.brian.wordle.userinput.getGuess
+import java.io.File
+
+
 
 suspend fun main() {
+    /*
     val apiClient = ApiClient()
     val getRandomWordsUseCase = GetRandomWordUseCase(apiClient)
+
 
     suspend fun getRandomWord(): String? {
         return when (val random = getRandomWordsUseCase.getRandomWord()) {
@@ -21,13 +26,20 @@ suspend fun main() {
         }
     }
 
-    val answer = getRandomWord() //Gets random word from API
+     */
+
+    val wordList = mutableListOf<String>()
+    File("src/main/kotlin/com/brian/wordle/data/words").readLines().forEach {
+        (wordList.add(it))
+    }
+    val answer = wordList.filter { it.length == 5 }.random()
+   // val answer = getRandomWord() //Gets random word from API
     val answerCharArray = answer?.toCharArray() // turns it into a character array
-    var userGuessCharArray = getGuess().toCharArray() // Gets guess from user input
+    var userGuessCharArray = getGuess(wordList).toCharArray() // Gets initial guess from user input
     var guessCount = 0
     val guessedCharsInAnswer = mutableListOf<Char>()
-    val totalGuessedChars = mutableListOf<Char>()
-    val answerBuilder = mutableListOf<Char>('_', '_', '_', '_', '_')
+    val guessedCharsNotInAnswer = mutableListOf<Char>()
+    val answerBuilder = mutableListOf('_', '_', '_', '_', '_')
 
 
     while (! userGuessCharArray.contentEquals(answerCharArray) && guessCount < 5) {
@@ -49,19 +61,19 @@ suspend fun main() {
                 answerBuilder.toString().replace(",", "").replace("[", "").replace("]", "").trim()
         )
 
-        userGuessCharArray.forEach { totalGuessedChars.add(it) } // creates list of all guessed characters
+        userGuessCharArray.forEach { guessedCharsNotInAnswer.add(it) } // creates list of all guessed characters
         /**
          * should subtract chars in answer from this list
          */
         guessedCharsInAnswer.forEach {
-            if( it in totalGuessedChars) {
-                totalGuessedChars.remove(it)
+            if( it in guessedCharsNotInAnswer) {
+                guessedCharsNotInAnswer.remove(it)
             }
         }
-        println("Characters not in the answer: ${totalGuessedChars.toSet()}")
+        println("Characters not in the answer: ${guessedCharsNotInAnswer.toSet()}")
         println("Characters in the answer: ${guessedCharsInAnswer.toSet()}") //to set gets rid of duplicates in list
         guessCount ++
-        userGuessCharArray = getGuess().toCharArray() // gets new answer from user
+        userGuessCharArray = getGuess(wordList).toCharArray() // gets new answer from user
     }
 
     if (userGuessCharArray.contentEquals(answerCharArray)) {
@@ -70,6 +82,5 @@ suspend fun main() {
         println("\nSorry, please try again")
         print("Answer was: $answer")
     }
-
 
 }
